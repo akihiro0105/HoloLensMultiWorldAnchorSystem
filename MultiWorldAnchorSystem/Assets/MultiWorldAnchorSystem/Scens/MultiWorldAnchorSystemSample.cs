49cc2b8c495607aac49c01ec918f052d7e3df0b6
晃弘 上山 <akihiro@hololab.co.jp>
2018-05-15 02:37:01 +0900

    first commit

---
+++ MultiWorldAnchorSystem/Assets/MultiWorldAnchorSystem/Scens/MultiWorldAnchorSystemSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.XR.WSA.Input;
+using MultiWorldAnchorSystem;
+
+// MultiWorldAnchorSystemのサンプル
+// Airtapで再設置処理開始
+public class MultiWorldAnchorSystemSample : MonoBehaviour {
+    public GameObject[] CenterAnchors;
+
+    private ModelPositionManager modelPositionManager;
+    // Use this for initialization
+    void Start () {
+        modelPositionManager = GetComponent<ModelPositionManager>();
+        modelPositionManager.InitModelPositionManager(CenterAnchors);
+
+        InteractionManager.InteractionSourceReleased += (obj) =>
+        {
+            modelPositionManager.StartSetting();
+            for (int i = 0; i < CenterAnchors.Length; i++)
+            {
+                modelPositionManager.TrackingImageTargetEvent(i, CenterAnchors[i].transform.position, CenterAnchors[i].transform.rotation);
+            }
+        };
+    }
+}
